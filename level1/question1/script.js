be150f6e74ef71b110a46220236d09ca713439da
ElineSR <elinesr@hotmail.com>
2019-04-14 23:53:43 +0200

    last commit for now
    Yeah

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,45 @@
 //Use RegEx to validate form
+///Not complete
+// missing email validation at least.
+//Unsure if all of this would be in one function or something like this?
+
+
+
+
+function validateForm() {
+  var x = document.forms["myForm"]["name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+   } 	
+}
+function validatelastName() {
+  var x = document.forms["myForm"]["last_name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+   } 	
+}
+
+function validatePhone(){
+    	y = document.getElementById("numb").value;
+		var phoneNo =/^([0-9]{8})$/;
+  			if (y === phoneNo){
+ 				return(true);
+ 			}else {
+ 				alert("Number must be 8 digits");
+ 			
+ 			}
+ 		}
+ 		
+function validateEmail(){
+	e = document.getElementById("numb").value;
+	var emailAdr =/^$/;
+  		if (e === emailAdr){
+ 			return(true);
+ 		}else {
+ 			alert("Enter a valid email address.");	
+ 		}
+ 	}
+ 	
+ 	
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,41 @@
 //Console.log out elements in JSON file
+// Made many attempts at this. This below is wrong.
+/* Missing:
+Should I have made the "video" Json into a var?
+It is connected to the HTML throught the script tag, but it probably needs activating somehow. Unsure of how?
+Unsure of how to loop further into the information. Found no examples of that. Got confused.
+
+ */
+var json = {
+"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+}
+
+var videoData = json.video;
+
+for (var i in videoData) {
+	var id = videoData[i].id;
+	var name = videoData[i].name;
+	console.log(id);
+	console.log(name);
+}
+
+
+/*
+(function(){
+	var myVideo = JSON.parse(video);
+	console.log(mydata[0].id)
+	console.log(mydata[0].name)
+	console.log(mydata[0].url)
+	console.log(mydata[0].author)
+})();
